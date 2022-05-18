f5a92ad16ee32336f19b0db46c7f1d520b482c87
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-05-18 08:33:05 +0700

    .

--- src/page/TestPage.vue
+++ src/page/TestPage.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="my-component">
+        {{test ? reverseMessage : 'hahahaha'}}
         <div>{{ reverseMessage }}</div>
+        <div>{{test}}</div>
+        <button @click="tesst()">Click</button>
+        <div v-bind:class="[isActive ? activeClass : errorClass]">hahahah</div>
     </div>
 </template>
 
@@ -9,14 +13,26 @@
         name: 'TestPage',
         data() {
             return {
-                message: 'this is text'
+                message: 'this is text',
+                test: true,
+                toggle: false,
+                isActive: true,
+                errorClass: 'text-danger',
+                activeClass: 'text-success',
             }
         },
         computed: {
             reverseMessage() {
                 return this.message.split('').reverse().join('')
             }
-        }
+        },
+        methods: {
+            tesst() {
+                this.test = !this.test
+                this.isActive = !this.isActive
+                console.log(this.test)
+            }
+        },
     }
 </script>
 
