2cb253b9de1e42b22eaf917b018639db08d97ce1
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-05-18 11:10:15 +0700

    handler Date

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,9 +2,10 @@
 <div>
     <Nav> </Nav>
     <div class="flex items-center justify-center">
-        <v-date-picker :attributes='attributes' class="w-3/4 mt-10" color="red" is-dark is-range mode='single' tint-color='#f142f4' v-model='selectedDate' :theme-styles='themeStyles' is-double-paned is-inline>
+        <v-date-picker  :attributes='attributes' class="w-3/4 mt-10" color="red" is-dark mode='single' tint-color='#f142f4' v-model="date"  is-double-paned is-inline>
         </v-date-picker>
     </div>
+    <button @click="handlerDate()">test</button>
 </div>
 </template>
 
@@ -19,38 +20,15 @@ export default {
             attributes: [{
                 key: 'today',
                 highlight: true,
-                dates: new Date(),
             }],
-            selectedDate: null,
-            themeStyles: {
-                wrapper: {
-                    border: '1',
-                },
-                header: {
-                    color: '#fafafa',
-                    backgroundColor: '#f142f4',
-                    borderColor: '#404c59',
-                    borderWidth: '1px 1px 0 1px',
-                },
-                headerVerticalDivider: {
-                    borderLeft: '1px solid #404c59',
-                },
-                weekdays: {
-                    color: '#ffffff',
-                    backgroundColor: '#f142f4',
-                    borderColor: '#ff0098',
-                    borderWidth: '0 1px',
-                    padding: '5px 0 10px 0',
-                },
-                weekdaysVerticalDivider: {
-                    borderLeft: '1px solid #404c59',
-                },
-                weeks: {
-                    border: '1px solid #dadada',
-                },
-            },
+            date: new Date(),
         };
-    }
+    },
+    methods: {
+        handlerDate() {
+            console.log(this.date)
+        },
+    },
 }
 </script>
 
